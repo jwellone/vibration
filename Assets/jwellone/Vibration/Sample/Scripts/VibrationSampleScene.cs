5b94419de3e2da4d6793abf2536dc6d546dba2b0
UsuiYasuhiko-jw1 <usui_yasuhiko@jwellone.co.jp>
2021-11-19 00:54:31 +0900

    format

--- Assets/jwellone/Vibration/Sample/Scripts/VibrationSampleScene.cs
+++ Assets/jwellone/Vibration/Sample/Scripts/VibrationSampleScene.cs
@@ -5,55 +5,55 @@ using jwellone.Device;
 
 namespace jwellone.Sample
 {
-    public class VibrationSampleScene : MonoBehaviour
-    {
-        [Serializable]
-        class SliderUI
-        {
-            [SerializeField] Text m_text;
-            [SerializeField] Slider m_slider;
+	public class VibrationSampleScene : MonoBehaviour
+	{
+		[Serializable]
+		class SliderUI
+		{
+			[SerializeField] Text m_text;
+			[SerializeField] Slider m_slider;
 
-            public float value => m_slider.value/10f;
+			public float value => m_slider.value / 10f;
 
-            public void Setup()
+			public void Setup()
 			{
-                m_slider.onValueChanged.AddListener(_ =>
-                {
-                    UpdateText();
-                });
+				m_slider.onValueChanged.AddListener(_ =>
+				{
+					UpdateText();
+				});
 
-                UpdateText();
+				UpdateText();
 			}
 
-            private void UpdateText()
+			private void UpdateText()
 			{
-                m_text.text = m_slider.name + ":" + value;
-            }
-        }
+				m_text.text = m_slider.name + ":" + value;
+			}
+		}
 
-        [SerializeField] SliderUI m_secondsSlider;
-        [SerializeField] SliderUI m_intensitySlider;
-        [SerializeField] SliderUI m_sharpnessSlider;
+		[SerializeField] SliderUI m_secondsSlider;
+		[SerializeField] SliderUI m_intensitySlider;
+		[SerializeField] SliderUI m_sharpnessSlider;
 
 		private void Awake()
 		{
-            m_secondsSlider.Setup();
-            m_intensitySlider.Setup();
-            m_sharpnessSlider.Setup();
+			m_secondsSlider.Setup();
+			m_intensitySlider.Setup();
+			m_sharpnessSlider.Setup();
 		}
 
 		public void OnClickPlay()
 		{
-            VibrationManager.Play(
-                m_secondsSlider.value,
-                m_intensitySlider.value,
-                m_sharpnessSlider.value);
+			VibrationManager.Play(
+				m_secondsSlider.value,
+				m_intensitySlider.value,
+				m_sharpnessSlider.value);
 		}
 
-        public void OnClickStop()
+		public void OnClickStop()
 		{
-            VibrationManager.Stop();
+			VibrationManager.Stop();
 		}
 
-    }
+	}
 }
